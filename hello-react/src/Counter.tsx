c151bd7a64d18dcd23fb962e44eb36cfdfd04b9d
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-05-14 17:06:20 +0200

    Jour 1

---
+++ hello-react/src/Counter.tsx
@@ -0,0 +1,31 @@
+import { useState, type ChangeEvent, type MouseEvent } from "react";
+
+function Counter() {
+  // const temp = useState(0);
+  // const count = temp[0];
+  // const setCount = temp[1];
+  //    [0    , function]
+  const [count, setCount] = useState(0);
+  const [list, setList] = useState<string[]>([]);
+
+  function handleClick(event: MouseEvent<HTMLButtonElement>) {
+    console.log(event.currentTarget.nodeName); // BUTTON
+    console.log('Button clicked');
+    setCount(count + 1);
+  }
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    console.log(event.currentTarget.nodeName); // INPUT
+    console.log(event.target.nodeName); // INPUT
+  }
+
+  return (
+    <div>
+      <h1>Counter: {count}</h1>
+      <button onClick={handleClick}>Increment</button>
+      <input type="text" onChange={handleChange} />
+    </div>
+  );
+}
+
+export default Counter;
