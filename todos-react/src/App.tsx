ed3738963e4cb31093566238fb25193709b6050c
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2025-05-15 11:12:07 +0200

    Correction Sans Immer

--- todos-react/src/App.tsx
+++ todos-react/src/App.tsx
@@ -1,20 +1,55 @@
 import './App.css';
-import type { ReactNode } from 'react';
+import {
+  useState,
+  type ChangeEvent,
+  type FormEvent,
+  type ReactNode,
+} from 'react';
 import TodoItem from './TodoItem';
+import type { Todo } from './todo';
 
 function App(): ReactNode {
-  const todos = [
+  const [todos, setTodos] = useState<Todo[]>([
     { id: 123, title: 'ABC', completed: false },
     { id: 456, title: 'DEF', completed: true },
     { id: 789, title: 'XYZ', completed: false },
-  ];
+  ]);
+  const [newTodo, setNewTodo] = useState('HIJ');
+
   const editingId = 789;
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setTodos([...todos, { id: Date.now(), title: newTodo, completed: false }]);
+    setNewTodo('');
+  }
+
+  function handleToggleClick(event: ChangeEvent<HTMLInputElement>) {
+    const isChecked = event.currentTarget.checked;
+    setTodos(
+      todos.map((todo) => {
+        if (todo.completed !== isChecked) {
+          return { ...todo, completed: isChecked };
+        }
+        return todo;
+      }),
+    );
+  }
 
   return (
     <>
-      <form className="todos-form">
-        <input type="checkbox" className="todos-toggle-checked" />
-        <input type="text" className="todos-new-input" />
+      <form className="todos-form" onSubmit={handleSubmit}>
+        <input
+          type="checkbox"
+          className="todos-toggle-checked"
+          onChange={handleToggleClick}
+        />
+        <input
+          type="text"
+          className="todos-new-input"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
         <button>+</button>
       </form>
       <div className="todos-container">
